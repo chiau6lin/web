81d4ef351989aca486d13349bd8f9fdcbfc6a39c
Jamie Lin <jamielin0.tw@gmail.com>
2023-10-24 11:09:24 +0800

    feat: implement Attributes class

---
+++ src/modules/Attributes.ts
@@ -0,0 +1,12 @@
+export class Attributes<T> {
+  constructor(public data: T) {}
+
+  get<K extends keyof T>(key: K): T[K] { // instead of => number | string | boolean 
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
+
