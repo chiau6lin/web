e97a12099f33cdaa60aabb8401e434fb912d53f0
Jamie Lin <jamielin0.tw@gmail.com>
2023-10-24 13:33:38 +0800

    refactor: refactor User Class to using inheritance in Model Class

--- src/modules/Model.ts
+++ src/modules/Model.ts
@@ -1,3 +1,5 @@
+import { AxiosPromise, AxiosResponse } from "axios";
+
 interface ModelAttributes<T> {
   set(value: T): void;
   get<K extends keyof T>(key: K): T[K];
@@ -5,8 +7,8 @@ interface ModelAttributes<T> {
 }
 
 interface Sync<T> {
-  fetch(id: number): Promise<T>;
-  save(data: T): Promise<T>;
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
 }
 
 interface Events {
@@ -14,10 +16,74 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-export class Model<T> {
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
   constructor(
-    private modelAttributes: ModelAttributes<T>,
+    private attributes: ModelAttributes<T>,
     private events: Events,
-    private Sync: Sync<T>
-  ) {}  
+    private sync: Sync<T>
+  ) {}
+  
+   /**
+   * refactor #1
+   * 
+   *    on(eventName: string, callback: Callback): void {
+   *      this.event.on(eventName, callback);
+   *    }
+   * 
+   * => But when Eventing funtion signature change,
+   * also need modify this. and other reson is also we need to call like this:
+   *
+   *    user.sync.save();
+   *    user.event.on(); 
+   * 
+   * So using `Getter accessors` 
+   * 
+   */
+   get on() {
+    return this.events.on.bind(this.events);
+  }
+
+  get trigger() {
+    return this.events.trigger.bind(this.events);
+  }
+  
+  get get() {
+    // console.log(user.get("name"));
+    // error ! root cause: `this`
+    // return this.attributes.get;
+
+    return this.attributes.get.bind(this.attributes);
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+
+    if (typeof id !== "number") {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      })
+  }
 }
--- src/modules/User.ts
+++ src/modules/User.ts
@@ -1,7 +1,7 @@
-import { AxiosResponse } from "axios";
 import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { ApiSync } from "./ApiSync";
+import { Model } from "./Model";
 
 interface UserProps {
   id?: number;
@@ -11,72 +11,12 @@ interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  public event: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  /**
-   * refactor #1
-   * 
-   *    on(eventName: string, callback: Callback): void {
-   *      this.event.on(eventName, callback);
-   *    }
-   * 
-   * => But when Eventing funtion signature change,
-   * also need modify this. and other reson is also we need to call like this:
-   *
-   *    user.sync.save();
-   *    user.event.on(); 
-   * 
-   * So using `Getter accessors` 
-   * 
-   */
-  get on() {
-    return this.event.on.bind(this.event);
-  }
-
-  get trigger() {
-    return this.event.trigger.bind(this.event);
-  }
-  
-  get get() {
-    // console.log(user.get("name"));
-    // error ! root cause: `this`
-    // return this.attributes.get;
-
-    return this.attributes.get.bind(this.attributes);
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.event.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-
-    if (typeof id !== "number") {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      })
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    )
   }
 }
